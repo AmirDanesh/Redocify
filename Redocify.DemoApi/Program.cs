3059f1ba31b0dda017ddf07439dd1197a963dc61
Amir Daneshvar <amir77daneshvar@gmail.com>
2025-04-26 10:34:02 +0330

    Update Program.cs

--- Redocify.DemoApi/Program.cs
+++ Redocify.DemoApi/Program.cs
@@ -10,11 +10,11 @@ var app = builder.Build();
 
 app.UseRedocify();
 // OR
-app.UseRedocify(opt =>
-{
-    opt.LaunchRoute = "";
-    opt.SwaggerUrl = "swagger/customSwagger.json";
-});
+//app.UseRedocify(opt =>
+//{
+//    opt.LaunchRoute = "";
+//    opt.SwaggerUrl = "swagger/customSwagger.json";
+//});
 
 app.UseAuthorization();
 app.MapControllers();
